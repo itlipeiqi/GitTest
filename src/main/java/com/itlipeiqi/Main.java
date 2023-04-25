f7a0996d3b7093b3883b8f094df9945c4053756a
lpq <lpq@qq.com>
2023-04-25 20:14:23 +0800

    two commit

--- src/main/java/com/itlipeiqi/Main.java
+++ src/main/java/com/itlipeiqi/Main.java
@@ -3,5 +3,6 @@ package com.itlipeiqi;
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello Git! I am idea");
+        System.out.println("two!you are roubianqi!");
     }
 }
